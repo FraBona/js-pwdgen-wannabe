140d7599121298d0efea54948a4cf48b4463eac2
FraBona <francescobonafede08@gmail.com>
2023-10-03 15:49:27 +0200

    commit

---
+++ js/app.js
@@ -0,0 +1,36 @@
+//- chidere all'utente il suo nome
+//  - creare variabile name
+let nome;
+//  - fare prompt dove chiedi il nome all'utente
+//  - assegnare a name il risultato del prompt
+nome = prompt("Ciao come ti chiami?");
+//  - stamparlo con il console.log
+console.log(nome);
+//
+//- chiedere all'utente il suo cognome
+//  - creare variabile username
+let username 
+//  - fare prompt dove chiedi il nome all'utente
+//  - assegnare a username il risultato del prompt
+username = prompt("Quale il tuo cognome?");
+//  - stamparlo con il console.log
+console.log(username);
+//
+//- chiedere all'utente il suo colore perferito
+//  - creare variabile color
+let color;
+//  - fare prompt dove chiedi il nome all'utente
+//  - assegnare a color il risultato del prompt
+color = prompt("Quale il tuo colore perferito");
+//  - stamparlo con il console.log
+console.log(color);
+//
+//- password generata dagli input dell'utente
+//  - creare variabile password dove concateni le variabili name username e color piu il numero 21
+let password = nome + username + color + 21
+// stamparlo con il console.log
+console.log(password)
+// creare variabile che andara a modificare il dom tramite id 
+const passwordDomElement = document.getElementById("password");
+// passare la variabile password a passwordDomElement
+passwordDomElement.innerHTML = password;
--- readme.md
+++ readme.md
@@ -1,6 +1,25 @@
 # JS-PWDGEN-WANNAEB
 
 - chidere all'utente il suo nome
+  - creare variabile name
+  - fare prompt dove chiedi il nome all'utente
+  - assegnare a name il risultato del prompt
+  - stamparlo con il console.log
+
 - chiedere all'utente il suo cognome
+  - creare variabile username
+  - fare prompt dove chiedi il nome all'utente
+  - assegnare a username il risultato del prompt
+  - stamparlo con il console.log
+
 - chiedere all'utente il suo colore perferito
+  - creare variabile color
+  - fare prompt dove chiedi il nome all'utente
+  - assegnare a color il risultato del prompt
+  - stamparlo con il console.log
+
 - password generata dagli input dell'utente
+  - creare variabile password dove concateni le variabili name username e color piu il numero 21
+  - stamparlo con il console.log
+  - creare variabile che andara a modificare il dom tramite id 
+  - passare la variabile password a passwordDomElement
